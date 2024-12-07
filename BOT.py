95191154c72f49bfda9bd8099d30e55bbcec3109
Marlon Luiz Portella <marlonluizportella@hotmail.com>
2024-12-07 11:49:42 -0300

    Logica base
    Logica descrita.

---
+++ BOT.py
@@ -0,0 +1,9 @@
+# 1 - abrir a planilha
+# 2 - ler a planilha
+# 3 - guardar os dados
+# 4 - organizar e mostrar os dados da planilha
+# 5 - verificar datas e organizar pela mais proxima
+# 6 - programar o horario para enviar a mensagem
+# 7 - Escrever e enviar a mensagem
+# 8 - ir para o proximo contato
+
